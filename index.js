db38a396fc70399c92a54066becc131904b629b6
Simon Lepel <simbo@simbo.de>
2015-08-31 00:05:39 +0200

    add module code

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = require('./lib/generator.js');
+module.exports.Config = require('./lib/config.js');
+module.exports.Option = require('./lib/option.js');
+module.exports.TemplateSet = require('./lib/templateset.js'),
+module.exports.Question = require('./lib/question.js');
