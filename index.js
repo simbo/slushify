6b5566420737497fb6e5c81dceaeea41666afddc
Simon Lepel <simbo@simbo.de>
2015-08-31 22:01:05 +0200

    fix main exports typo

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 module.exports = require('./lib/generator.js');
 module.exports.Config = require('./lib/config.js');
 module.exports.Option = require('./lib/option.js');
-module.exports.TemplateSet = require('./lib/templateset.js'),
+module.exports.TemplateSet = require('./lib/templateset.js');
 module.exports.Question = require('./lib/question.js');
